7a114a80c6b0856ac8406b00a9ff9fe124ef0d3f
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-26 18:36:51 +0300

    Replaced planeType List to PlaneType folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { Route } from '../../node_modules/@angular/compiler/src/core';
 import { componentRefresh } from '../../node_modules/@angular/core/src/render3/instructions';
-import { PlaneTypeListComponent } from './plane-type-list/plane-type-list.component';
+import { PlaneTypeListComponent } from './PlaneType/plane-type-list/plane-type-list.component';
 import { PlaneTypeDetailsComponent } from './PlaneType/plane-type-details/plane-type-details.component';
 import { PlaneTypeService } from './PlaneType/Service/plane-type.service';
 import { HomeComponent } from './home/home.component';
